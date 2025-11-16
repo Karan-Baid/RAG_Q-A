a21c98c755cd59e1ad91db92c899e9fecda1f9f2
Karan-Baid <151212469+Karan-Baid@users.noreply.github.com>
2025-11-16 22:31:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain.chains.retrieval import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains .retrieval import create_retrieval_chain
+from langchain_classic.chains .combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 import os
 from langchain_groq import ChatGroq
